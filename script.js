cab7a8b47b65fc2ffa94ab6b9fd1a3fb49ad0a13
clementD99 <clement.desmur@gmail.com>
2025-11-20 14:48:12 +0100

    Code pour l'intégration des cases en HTML

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-// code js 
+// Pour afficher les cards en HTML
+
+const cardsList = document.getElementById("cards");
+let cards = [];
+
+const fetchCards = async () => {
+  try {
+    const response = await fetch("cards.json");
+    const cards = await response.json();
+    return cards;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+function displayCards(cards) {
+  cardsList.innerHTML = "";
+  cards.forEach((card) => {
+    const element = document.createElement("div");
+    element.classList.add("articles");
+    element.dataset.name = card.name;
+
+    element.innerHTML = `
+    <div class="front">
+    <img src="${card.image}" alt="${card.title}">
+    <div class ="details">
+    <h3>${card.number}</h3>
+    <h4>${card.title}</h4>
+    <p>${card.description}</p>
+    </div>
+    </div>
+    `;
+    cardsList.appendChild(element);
+  });
+}
+
+fetchCards().then((cards) => {
+  if (cards) {
+    element = cards;
+    displayCards(element);
+  }
+});
